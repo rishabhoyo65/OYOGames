ab2585ee260decce397ee21266fca68e6d262c6a
Shubham Goyal <shubham.goyal3@oyorooms.com>
2021-06-10 15:40:06 +0530

    Added: Leaderboard dummy data

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import "./home.scss";
 import Navbar from "../common/navbar/Navbar";
+import Leaderboard from "../leaderboard/Leaderboard";
 
 export default function Home() {
     return (
         <div className="page">
             <Navbar/>
             <div className="page-main">
-
+                <Leaderboard />
             </div>
         </div>
     )
--- src/components/leaderboard/leaderboard.js
+++ src/components/leaderboard/leaderboard.js
@@ -5,8 +5,13 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-
+import { makeStyles,withStyles } from '@material-ui/core/styles';
+const users = [
+    { rank:1,username:"hello",score:34},
+    { rank:2,username:"hello",score:34},
+    { rank:3,username:"hello",score:34},
+    { rank:4,username:"hello",score:34}
+];
 const triangle = {
     width: "0",
     height: "0",
@@ -15,7 +20,7 @@ const triangle = {
     borderTop: "75px solid red"
 };
 const rectangle = {
-    width: "6000px",
+    width: "600px",
     height: "100px",
     backgroundColor: "red"
 };
@@ -58,20 +63,19 @@ export default function Leaderboard(props) {
     const classes = useStyles();
 
     return (
-        <div className="leaderboard-table">
+        <div className="leaderboard-table" style={{ width: "600px"}}>
             <LeaderboardHead />
             <TableContainer component={Paper}>
                 <Table className={classes.table} aria-label="simple table">
                     <TableBody>
-                    {props.users.map(user => {
-                        const color = getColor(user.rank);
+                    {users.map(user => (
                         <StyledTableRow key={user.rank} style={{ position: "relative" }}>
-                            <hr style={{ position: "absolute",top: "49px",eft: "48px",width: "510px",
-                                        borderBottom: "2px solid " + getColor(index)}}
+                            <hr style={{ position: "absolute",top: "49px",left: "48px",width: "510px",
+                                        borderBottom: "2px solid " + getColor(user.rank)}}
                             />
 
                             <StyledTableCell  style={{ width: 100 }}component="th" scope="row">
-                                <div style={{position: "relative",backgroundColor: color,
+                                <div style={{position: "relative",backgroundColor: getColor(user.rank),
                                             borderRadius: "100%",width: "50px",height: "50px"}}>
                                     <div style={{
                                         position: "absolute",left: "50%",top: "50%",
@@ -85,7 +89,7 @@ export default function Leaderboard(props) {
                             <StyledTableCell style={{ width: 400 }} >{user.username}</StyledTableCell>
                             <StyledTableCell style={{ width: 100 }} >{user.score}</StyledTableCell>
                         </StyledTableRow>
-                    })}
+                    ))}
                     </TableBody>
                 </Table>
             </TableContainer>
@@ -94,7 +98,7 @@ export default function Leaderboard(props) {
 }
 
 const LeaderboardHead = function(){
-    return <div style={{ position: "relative" }}>
+    return <div style={{ position: "relative"}}>
         <h1 style={{
             position: "absolute",
             left: "50%",
