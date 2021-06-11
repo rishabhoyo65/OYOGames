dc702f1157fa9226a62945aad9c38403210bc9f0
OYO <oyo@Bs-MacBook-Pro.local>
2021-06-11 14:52:58 +0530

    slider updated

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -7,15 +7,15 @@ export default () => (
   <Carousel autoPlay showThumbs={false}>
     <div>
       <img alt="" src={IMAGE_PATH + "Commingsoon.png"} style={{height : '400px'}}/>
-      <p className="legend">Legend 1</p>
+      <p className="legend">Welcome</p>
     </div>
     <div>
       <img alt="" src={IMAGE_PATH + "slider1.png"} style={{height : '400px'}}/>
-      <p className="legend">Legend 2</p>
+      <p className="legend">Play & Earn Points</p>
     </div>
     <div>
-      <img alt="" src={IMAGE_PATH + "slider1.png"} style={{height : '400px'}}/>
-      <p className="legend">Legend 3</p>
+      <img alt="" src={IMAGE_PATH + "gamezone.png"} style={{height : '400px'}}/>
+      <p className="legend">Win Amazing Prizes</p>
     </div>
     
   
